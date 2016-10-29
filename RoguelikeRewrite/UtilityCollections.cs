d8790a252c5f09e865f16a4d644033dfdaa0f00f
derrickcreamer <derrickcreamer@gmail.com>
2016-10-28 23:31:35 -0400

    Updates to UtilityCollections, including XML comments

--- RoguelikeRewrite/UtilityCollections.cs
+++ RoguelikeRewrite/UtilityCollections.cs
@@ -3,8 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace UtilityCollections {
+namespace UtilityCollections { // Updated 2016-10-25
+	/// <summary>
+	/// A hashset with an indexer for convenience.
+	/// </summary>
 	public class DefaultHashSet<T> : HashSet<T> {
+		/// <summary>
+		/// GET: Returns true if the given element is present.
+		/// SET: If 'true', add the given element. If 'false', remove the given element.
+		/// </summary>
 		public bool this[T t] {
 			get { return Contains(t); }
 			set {
@@ -16,16 +23,32 @@ namespace UtilityCollections {
 		public DefaultHashSet(IEqualityComparer<T> comparer) : base(comparer) { }
 		public DefaultHashSet(IEnumerable<T> collection, IEqualityComparer<T> comparer = null) : base(collection, comparer) { }
 	}
+	/// <summary>
+	/// A dictionary that returns a default value if the given key isn't present.
+	/// </summary>
 	public class DefaultValueDictionary<TKey, TValue> : Dictionary<TKey, TValue> {
 		new public TValue this[TKey key] {
 			get {
 				TValue v;
-				TryGetValue(key, out v); //TryGetValue sets its out parameter to default if not found.
-				return v;
+				if(TryGetValue(key, out v) || getDefaultValue == null) { // TryGetValue sets 'v' to default(TValue) if not found.
+					return v;
+				}
+				else return getDefaultValue();
 			}
 			set {
 				base[key] = value;
 			}
+		}
+		private Func<TValue> getDefaultValue = null;
+		/// <summary>
+		/// If defined, the result of this method will be used instead of default(TValue).
+		/// </summary>
+		public Func<TValue> GetDefaultValue {
+			get {
+				if(getDefaultValue == null) return () => default(TValue);
+				else return getDefaultValue;
+			}
+			set { getDefaultValue = value; }
 		}
 		public DefaultValueDictionary() { }
 		public DefaultValueDictionary(IEqualityComparer<TKey> comparer) : base(comparer) { }
@@ -48,9 +71,17 @@ namespace UtilityCollections {
 			d = new Dictionary<TKey, ICollection<TValue>>(comparer);
 			this.createCollection = createCollection;
 		}
+		/// <summary>
+		/// Create a MultiValueDictionary that uses a specific type of ICollection, instead of the default List.
+		/// </summary>
+		/// <typeparam name="TCollection">The type of ICollection to use instead of List</typeparam>
 		public static MultiValueDictionary<TKey, TValue> Create<TCollection>() where TCollection : ICollection<TValue>, new() {
 			return new MultiValueDictionary<TKey, TValue>(() => new TCollection());
 		}
+		/// <summary>
+		/// Create a MultiValueDictionary that uses a specific type of ICollection, instead of the default List.
+		/// </summary>
+		/// <typeparam name="TCollection">The type of ICollection to use instead of List</typeparam>
 		public static MultiValueDictionary<TKey, TValue> Create<TCollection>(IEqualityComparer<TKey> comparer)
 			where TCollection : ICollection<TValue>, new()
 		{
@@ -78,7 +109,8 @@ namespace UtilityCollections {
 		}
 		public IEnumerable<TValue> this[TKey key] {
 			get {
-				if(d.ContainsKey(key)) return d[key];
+				ICollection<TValue> values;
+				if(d.TryGetValue(key, out values)) return values;
 				else return Enumerable.Empty<TValue>();
 			}
 			//todo: xml: This one replaces the entire contents of this key.
@@ -96,28 +128,45 @@ namespace UtilityCollections {
 			}
 		}
 		public void Add(TKey key, TValue value) {
-			if(!d.ContainsKey(key)) d.Add(key, createCollection());
-			d[key].Add(value);
+			ICollection<TValue> values;
+			if(!d.TryGetValue(key, out values)) {
+				values = createCollection();
+				d.Add(key, values);
+			}
+			values.Add(value);
 		}
 		public bool Remove(TKey key, TValue value) {
-			if(d.ContainsKey(key)) return d[key].Remove(value);
+			ICollection<TValue> values;
+			if(d.TryGetValue(key, out values)) return values.Remove(value);
 			else return false;
 		}
 		public void Clear() { d.Clear(); }
 		public void Clear(TKey key) { d.Remove(key); }
-		public bool Contains(TKey key, TValue value) => d.ContainsKey(key) && d[key].Contains(value);
+		public bool Contains(TKey key, TValue value) {
+			ICollection<TValue> values;
+			return d.TryGetValue(key, out values) && values.Contains(value);
+		}
 		public bool Contains(TValue value) {
-			foreach(var list in d.Values) {
-				if(list.Contains(value)) return true;
+			foreach(var collection in d.Values) {
+				if(collection.Contains(value)) return true;
 			}
 			return false;
 		}
 		public bool AddUnique(TKey key, TValue value) {
-			if(Contains(key, value)) return false;
-			if(!d.ContainsKey(key)) d.Add(key, createCollection());
-			d[key].Add(value);
+			ICollection<TValue> values;
+			if(d.TryGetValue(key, out values)) {
+				if(values.Contains(value)) return false;
+			}
+			else {
+				values = createCollection();
+				d.Add(key, values);
+			}
+			values.Add(value);
 			return true;
 		}
-		public bool AnyValues(TKey key) => d.ContainsKey(key) && d[key].Any();
+		public bool AnyValues(TKey key) {
+			ICollection<TValue> values;
+			return d.TryGetValue(key, out values) && values.Any();
+		}
 	}
 }
